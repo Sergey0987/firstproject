aa11b9b2db9366ab65589a692e41d4e04203ae2d
Slivochnikov Sergey <serjslivochnikov@yandex.ru>
2018-07-04 20:21:38 +0300

    new

---
+++ Функции 4/123.py
@@ -0,0 +1,9 @@
+def orten(argument1):
+    global argum
+    argum = argum + 1
+    print(argum)
+    return argum
+
+argum = 5
+orten(argum)
+print(argum)
--- Функции 4/Обратная польская нотация.py
+++ Функции 4/Обратная польская нотация.py
@@ -1,12 +1,22 @@
-def OPN(list_from_string):
+def main():    
+    global list_from_string
+    global stek
+    list_from_string = input().split()
     stek = []
-    result = 0
+    fromStrToInt(list_from_string)
+    reversePolishNotation(list_from_string)
+    print(stek[0]) 
+
+def fromStrToInt(argument):
+    global list_from_string
     for i in range(len(list_from_string)):
         try:
             list_from_string[i] = int(list_from_string[i])
         except ValueError:
             list_from_string[i] = list_from_string[i]
 
+def reversePolishNotation(argument1):
+    result = 0
     for i in range(len(list_from_string)):
         if list_from_string[i] == '*':
             index=len(stek)-2
@@ -26,7 +36,5 @@ def OPN(list_from_string):
             stek.insert(index, result)
         else:
             stek.append(list_from_string[i])
-    print(stek[0])
-
-list_from_string = input().split()
-OPN(list_from_string)
+   
+main()
